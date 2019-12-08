1afe54e98c55dcaf1c02ad16f0246cf22c6cff40
winjeg <winjeg@qq.com>
2019-12-08 21:29:45 +0800

    add response wrapper

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
-module "github.com/winjeg/toy"
+module github.com/winjeg/toy
+
+go 1.13
---
+++ resp/resp.go
@@ -0,0 +1,57 @@
+package resp
+
+import (
+	"fmt"
+	"strings"
+)
+
+type responseWrapper struct {
+}
+
+const (
+	SimpleStrPrefix = "+"
+	ErrorStrPrefix  = "-"
+	IntegerPrefix   = ":"
+	BulkStrPrefix   = "$"
+	ArrayPrefix     = "*"
+	EndStr          = "\r\n"
+	NullBulkStr     = "$-1\r\n"
+	EmptyArray      = "*0\r\n"
+)
+
+func (rw *responseWrapper) SimpleString(str string) string {
+	return SimpleStrPrefix + str + EndStr
+}
+
+func (rw *responseWrapper) ErrorString(str string) string {
+	return ErrorStrPrefix + str + EndStr
+}
+
+func (rw *responseWrapper) IntegerStr(val int64) string {
+	return IntegerPrefix + fmt.Sprintf("%d", val) + EndStr
+}
+
+func (rw *responseWrapper) BulkStr(str string) string {
+	lenStr := fmt.Sprintf("%d", len(str))
+	return BulkStrPrefix + lenStr + EndStr + str + EndStr
+}
+
+func (rw *responseWrapper) ArrayStr(elements []interface{}) string {
+	if len(elements) == 0 {
+		return EmptyArray
+	}
+	var builder strings.Builder
+	builder.WriteString(ArrayPrefix)
+	builder.WriteString(fmt.Sprintf("%d", len(elements)))
+	builder.WriteString(EndStr)
+	for i := range elements {
+		switch elements[i].(type) {
+		case int64:
+			builder.WriteString(rw.IntegerStr(elements[i].(int64)))
+			break
+		default:
+			builder.WriteString(rw.BulkStr(elements[i].(string)))
+		}
+	}
+	return builder.String()
+}
